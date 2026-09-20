8ee70695d3e0fb2f2b4c3a2dd95980d6789f43d3
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2026-09-20 14:13:01 +0300

    package upgrades

--- Library/Constants.cs
+++ Library/Constants.cs
@@ -4,6 +4,6 @@ public static class Constants
 {
     public const string ClientName = "OpenSubtitlesCliBulk";
     public const string BaseUrl = "https://api.opensubtitles.com/api/v1";
-    public const string ApiKey = "";
+    public const string ApiKey = "zOmbyh6jLDWWwLYDgqSYpwcw9Ava3WC3";
     public const string UserAgent = "openapi-bulk-net v0.1";
 }
